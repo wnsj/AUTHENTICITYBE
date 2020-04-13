c32481ef478d7c04a9917ed80a46ce0b04de99a6
sunyl-5664 <lx5664@hotmail.com>
2020-04-13 14:01:56 +0800

    test

--- build-store-web/src/router/index.js
+++ build-store-web/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import conditionSearch from '@/components/conditionSearch.vue'
+import conditionSearch from '@/components/ConditionSearch/conditionSearch.vue'
 Vue.use(Router)
 
 export default new Router({
