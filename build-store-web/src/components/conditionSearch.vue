84199b69c8ea9592af87642c3d64a1c9fe777623
sunyl-5664 <lx5664@hotmail.com>
2020-04-13 13:20:48 +0800

    1、框架搭建 2、条件筛选（请求接口）

---
+++ build-store-web/src/components/conditionSearch.vue
@@ -0,0 +1,38 @@
+<template>
+</template>
+
+<script>
+  import axios from 'axios'
+  export default {
+    name: "conditionSearch",
+    data() {
+      return {
+        searchForm: {}
+      };
+    },
+    methods: {
+      // 条件查询
+      searchByCondition () {
+        let url = 'http://localhost:8080/buildingBean/getAllBuildByPage'
+        axios.post(url, this.searchForm).then(function (response) {
+          if (response.data.retCode === '0000') {
+            if (response.data.retData.records.length > 0) {
+              return response.data.retData
+            } else {
+            alert(response.data.retMsg)
+          }
+        }
+        }).catch(function (error) {
+          console.log(error);
+        });
+      }
+  },
+    mounted: function () {
+      this.searchByCondition()
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
